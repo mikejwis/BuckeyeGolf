d2533f6a670d1dde48c8c5f3b1a4f9eb239d6e89
Mike Wisniewski <michael.john.wisniewski@gmail.com>
2016-08-13 12:58:45 -0400

    Safari bootstrap loading fix

--- BuckeyeGolf/App/bootstrapLoader.js
+++ BuckeyeGolf/App/bootstrapLoader.js
@@ -1,7 +1,11 @@
 ﻿'use strict';
 
 //Bootstrap angular instead of including in html to allow google charts to load first
+
 google.setOnLoadCallback(function () {
-    angular.bootstrap(document.body, ['app']);
+    angular.bootstrap($('body'), ['app']);
 });
+
 google.load('visualization', '1', { packages: ['corechart', 'line'] });
+
+
