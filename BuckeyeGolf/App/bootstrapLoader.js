fedff06090349c654106423d37b85a9bf82a0b6b
Mike Wisniewski <michael.john.wisniewski@gmail.com>
2016-08-15 10:34:19 -0400

    Bootstrap bug fix

--- BuckeyeGolf/App/bootstrapLoader.js
+++ BuckeyeGolf/App/bootstrapLoader.js
@@ -3,7 +3,7 @@
 //Bootstrap angular instead of including in html to allow google charts to load first
 
 google.setOnLoadCallback(function () {
-    angular.bootstrap($('body'), ['app']);
+    //angular.bootstrap($('body'), ['app']);
 });
 
 google.load('visualization', '1', { packages: ['corechart', 'line'] });
