4ac4fcc9dc924858cab6d9b1f0481be635a46e18
Mike Wisniewski <michael.john.wisniewski@gmail.com>
2016-08-18 19:56:01 -0400

    Added TaskRunner for SASS

---
+++ BuckeyeGolf/gulpfile.js
@@ -0,0 +1,16 @@
+﻿/// <binding BeforeBuild='sass' />
+
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+gulp.task('sass', function () {
+    gulp.src('Content/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('Content'));
+});
